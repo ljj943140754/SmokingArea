fe93573a8914a2ea7b8bd868594714fdc7391438
Ljj_pro <liujinjiang@IT.com>
2019-11-13 11:14:41 +0800

    添加个封装 feedbackVO 的实体类，用于保存微信头像和名字

---
+++ src/main/java/com/smokeroom/entity/ext/FeedbackVO.java
@@ -0,0 +1,21 @@
+package com.smokeroom.entity.ext;
+
+import com.smokeroom.entity.Feedback;
+
+public class FeedbackVO extends Feedback{
+	private String ur_nickname;	
+	private String ur_avatarurl;
+	public String getUr_nickname() {
+		return ur_nickname;
+	}
+	public void setUr_nickname(String ur_nickname) {
+		this.ur_nickname = ur_nickname;
+	}
+	public String getUr_avatarurl() {
+		return ur_avatarurl;
+	}
+	public void setUr_avatarurl(String ur_avatarurl) {
+		this.ur_avatarurl = ur_avatarurl;
+	}
+	
+}
