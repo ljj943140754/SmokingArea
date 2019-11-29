6c922aa3c5ebaefb5ed7a4f27b874654baa5e44d
Lenovo <Lenovo@windows10.microdone.cn>
2019-11-29 11:59:55 +0800

    改变巡更任务进度文件夹名称

--- src/main/resources/static/page/taskReport/taskReport.js
+++ src/main/resources/static/page/taskReport/taskReport.js
@@ -11,43 +11,33 @@
 		  laydate.render({
 		    elem: '#test1' //指定元素
 		  });
-		
-	  $scope.redact=()=>{
-			 var checkStatus = table.checkStatus('facilityTable'); //idTest 即为基础参数 id 对应的值
-			 var da =  checkStatus.data[0];
-			 goToUrl( "#!/facility_update",'get', JSON.stringify(da) );
-		 }
-		 
-	 function goToUrl(url,method,data){
-	        var form = document.createElement("form");
-	        form.action = url;
-	        form.method = method;
-	        form.style.display = "none";
-	        document.body.appendChild(form);
-	        sessionStorage.setItem( "facilitydata" , data);
-	        form.submit();
-	        return form;
-	    }
+		  $scope.checklogin=function(){
+				form.render('select');
+				$.get("worker/checklogin.action",function(rs){
+				});
+				sessionStorage.removeItem('facilitydata');
+				sessionStorage.removeItem('taskdata');
+			}
+		  $scope.checklogin();
 	 $scope.query=()=>{
-		 let model = form.val("facilityform");
+		 let model = form.val("taskReportform");
 			 tb_instance.reload({
-				 url: 'facility/findFacility.action',
+				 url: 'report/getReport.action',
 				 where: { 
-					 fy_type:model.fy_type,
-					 fy_name:model.fy_name,
-					 fy_isdel:model.fy_isdel,
-					// fy_creation:model.fy_creation,
+					// rp_creation:model.rp_creation,
+					 rp_worker_id:model.rp_worker_id,
+					 rp_state:model.rp_state,
 				 } 
 			 });
 	 }
 		 
 		//删除表格。
 	    $scope.deleteById=function(){
-			var checkStatus = table.checkStatus('facilityTable'); //idTest 即为基础参数 id 对应的值
+			var checkStatus = table.checkStatus('taskReportTable'); //idTest 即为基础参数 id 对应的值
 			if( checkStatus.data.length > 0){
 				var da =  checkStatus.data[0];
 				 layer.confirm('确认删除吗', function(index){
-					 $.get("facility/delete.action",da,function(res){
+					 $.get("report/deleteReport.action",da,function(res){
 							layui.layer.msg("删除成功");
 							//表格重载。
 							 tb_instance.reload();
@@ -55,7 +45,23 @@
 				}); 
 			}
 		}
-		
+	    var reportID = {};
+	    $scope.getNameById=function(ID){
+	    	var  wk_name;
+	    	reportID.wk_num = ID;
+	    	$.ajax({
+	    		   type: "POST",
+	    		   url: "worker/getList.action",
+	    		   data: reportID,
+	    		   async : false,
+	    		   success: function(res){
+		    		if( res.data.list.length != 0 ){
+		    			 wk_name =  res.data.list[0].wk_name;
+		    		}
+	    		   }
+	    		});
+	    	return wk_name;
+		 }
 		//商品表格。
 		 tb_instance = table.render({
 			elem : '#taskReportTable',
@@ -68,56 +74,47 @@
                 ,limitName: 'pageSize' //每页数据量的参数名，默认：limit
             },
             parseData: function (res) { //将原始数据解析成 table 组件所规定的数据
-                console.log(res);
   				return {
                       "code": res.code, //解析接口状态
                       "count": res.data.total,
                       "data": res.data.list //解析数据列表
                   };
               },
-			url : 'facility/findFacility.action', //数据接口
+			url : 'report/getReport.action', //数据接口
 			cols : [ [ //表头
 			{
 				 type:"radio",
 				 width:40
 			}, 
 			{
-				field : 'fy_lat',
-				title : '维度',
+				field : 'rp_id',
+				title : '任务报表ID',
 			},
 			{
-				field : 'fy_lng',
-				title : '经度',
+				field : 'rp_title',
+				title : '报告标题',
 			}, {
-				field : 'fy_type',
-				title : "类型" ,
-				templet:function(d){
-				    if(d.fy_type =="1")return "吸烟亭";
-				    else  if(d.fy_type =="2")return "吸烟柱";
-				    else  if(d.fy_type =="3")return "售烟点";
-				    }
+				field : 'rp_content',
+				title : "报告内容" ,
 			}, {
-				field : 'fy_name',
-				title : "位置名称",
+				field : 'rp_vedio',
+				title : "报告图片/VR",
 				
 			}, {
-				field : 'fy_detail',
-				title : "位置明细",	
-			}, {
-				field : 'fy_res_link',
-				title : "【VR/封面】",	
-			}, {
-				field : 'fy_creation',
-				title : "创建时间",	
-			}, {
-				field : 'fy_createdby',
-				title : "创建者id",	
-			}, {
-				field : 'fy_isdel',
-				title : "是否启用",	
+				field : 'rp_worker_id',
+				title : "工作人员",
 				templet:function(d){
-				    if(d.fy_isdel =="0")return "未启用";
-				    else  if(d.fy_isdel =="1")return "启用";
+				    return $scope.getNameById( d.rp_worker_id );
+				    }
+			}, {
+				field : 'rp_creation',
+				title : "报告时间",	
+			}, {
+				field : 'rp_state',
+				title : "处理状态",	
+				templet:function(d){
+				    if(d.rp_state =="1")return "已处理";
+				    else  if(d.rp_state =="0")return "未处理";
 				    }
 			}] ]
 		});
