e8936f0e54b9a9084e4a8b0a5a7616f1b35e746a
Ljj_pro <liujinjiang@IT.com>
2019-12-03 14:48:07 +0800

    修改 pom 报错问题

--- src/main/java/com/AppStartUp.java
+++ src/main/java/com/AppStartUp.java
@@ -2,7 +2,7 @@ package com;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*吸烟亭项目（练手项目）*/
 @SpringBootApplication
 public class AppStartUp {
 	public static void main(String[] args) {
